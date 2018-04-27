0577151266603ea0991c3da56ba1dc980cea0d13
Spenser Andreassen <spenserca@gmail.com>
2018-04-26 19:35:05 -0500

    Adding more test cases

--- test/unit/specs/components/WordCount.spec.js
+++ test/unit/specs/components/WordCount.spec.js
@@ -2,10 +2,41 @@ import Vue from 'vue';
 import WordCount from '@/components/WordCount';
 
 describe('WordCount.vue', () => {
+
   describe('Initial data', () => {
-    it('should have default data', () => {
-      const defaultText = WordCount.data().text;
-      expect(defaultText).toEqual('');
+    it('should have default text', () => {
+      const text = WordCount.data().text;
+      expect(text).toEqual('');
+    });
+
+    it('should have default appendedText', () => {
+      const appendedText = WordCount.data().appendedText;
+      expect(appendedText).toEqual('');
+    });
+
+    it('should have default values', () => {
+      const values = WordCount.data().values;
+      expect(values).toEqual([]);
+    });
+
+    it('should have default labels', () => {
+      const labels = WordCount.data().labels;
+      expect(labels).toEqual([]);
+    });
+
+    it('should have default hasWords', () => {
+      const hasWords = WordCount.data().hasWords;
+      expect(hasWords).toEqual(false);
+    });
+  });
+
+  describe('UI', () => {
+    it('should not show the pie chart if no data exists', () => {
+
+    });
+
+    it('should show the pie chart if data exists', () => {
+
     });
   });
 
